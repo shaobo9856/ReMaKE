b91683ffea8f00d8012c257f90eab3a44b96c2b0
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-05-14 01:35:18 +0800

    test enth

--- run_bizsre.py
+++ run_bizsre.py
@@ -62,12 +62,12 @@ if __name__ == "__main__":
                 if lang1 == 'en' and lang2 == 'th':
                     with open(os.path.join("./data/mzsRE/",f"mzsre_test_duplicate_{lang1}{lang2}.json"), "r", encoding="utf-8") as f:
                         test_data = json.load(f)
-                if lang1 == 'en' and lang2 == 'ru':
-                    with open(os.path.join("./data/mzsRE/",f"mzsre_test_duplicate_{lang1}{lang2}.json"), "r", encoding="utf-8") as f:
-                        test_data = json.load(f)
-                if lang1 == 'en' and lang2 == 'de':
-                    with open(os.path.join("./data/mzsRE/",f"mzsre_test_duplicate_{lang1}{lang2}.json"), "r", encoding="utf-8") as f:
-                        test_data = json.load(f)
+                # if lang1 == 'en' and lang2 == 'ru':
+                #     with open(os.path.join("./data/mzsRE/",f"mzsre_test_duplicate_{lang1}{lang2}.json"), "r", encoding="utf-8") as f:
+                #         test_data = json.load(f)
+                # if lang1 == 'en' and lang2 == 'de':
+                #     with open(os.path.join("./data/mzsRE/",f"mzsre_test_duplicate_{lang1}{lang2}.json"), "r", encoding="utf-8") as f:
+                #         test_data = json.load(f)
                 # if lang1 == 'en' and lang2 == 'vi':
                 #     with open(os.path.join("./data/mzsRE/",f"mzsre_test_duplicate_{lang1}{lang2}.json"), "r", encoding="utf-8") as f:
                 #         test_data = json.load(f)
